fb463b9ff8eeb22e16f5f178fb04f1396809906d
TheHitMan7 <krtik.vrma@gmail.com>
2018-08-12 18:18:24 +0530

    Initial commit

---
+++ tmp_run.sh
@@ -0,0 +1,26 @@
+#!/system/bin/sh
+
+BBMGSK="/data/adb/magisk/busybox"
+BBSYS="/system/xbin/busybox"
+
+if [ -e $BBMGSK ] || [ -e $BBSYS ]; then
+if [ -e $BBMGSK ]; then
+    echo "busybox found in :" $BBMGSK
+	$BBMGSK mount -t ext4 -o remount,rw /system
+elif [ -e $BBSYS ]; then
+      echo "busybox found in :" $BBSYS
+      $BBSYS mount -t ext4 -o remount,rw /system
+fi;
+fi;
+ADDON="/system/addon.d"
+if [ -d $ADDON ]; then
+    echo "found in :" $ADDON
+else
+    echo "not found in :" $ADDON
+	echo " "
+	echo "Creating..."
+	mkdir /system/addon.d
+	echo "Set permission..."
+	chmod 0755 /system/addon.d
+	echo "Done :" $ADDON
+fi;
